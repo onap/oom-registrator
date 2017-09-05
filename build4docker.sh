acbfa0581a27a09de3fe4a4e80714c3603fbe381
HuabingZhao <zhao.huabing@zte.com.cn>
2017-09-05 15:01:14 +0800

    Add docker image build script
    Issue-Id: OOM-61
    Change-Id: I268edc39031a92e4519d762a81a418412da2a2e0
    Signed-off-by: HuabingZhao <zhao.huabing@zte.com.cn>

---
+++ build4docker.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+#
+# Copyright 2017 ZTE, Inc. and others.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+
+DIRNAME=`dirname $0`
+RUNHOME=`cd $DIRNAME/; pwd`
+echo @RUNHOME@ $RUNHOME
+
+#copy
+
+RELEASE_BASE_DIR=$RUNHOME/release
+
+RELEASE_DIR=${RELEASE_BASE_DIR}/registrator
+DOCKER_RUN_NAME=kube2msb
+DOCKER_IMAGE_NAME=kube2msb
+DOCKER_RELEASE_VERSION=latest
+DOCKER_REPOSITORY=nexus3.onap.org:10003
+
+rm -rf $RELEASE_DIR
+mkdir  $RELEASE_DIR -p
+
+
+cp -r $RUNHOME/target/* ${RELEASE_DIR}
+cp  $RUNHOME/build/docker/* ${RELEASE_DIR}
+#build docker image
+cd ${RELEASE_DIR}
+chmod 777 build_docker_image.sh
+
+docker rm -f ${DOCKER_RUN_NAME}
+docker rmi ${DOCKER_REPOSITORY}/onap/oom/${DOCKER_IMAGE_NAME}:${DOCKER_RELEASE_VERSION}
+
+
+./build_docker_image.sh -n=${DOCKER_REPOSITORY}/onap/oom/${DOCKER_IMAGE_NAME} -v=${DOCKER_RELEASE_VERSION} -d=./docker
+
+#docker run
+
+docker run -d --net=host  --name ${DOCKER_RUN_NAME} ${DOCKER_REPOSITORY}/onap/oom/${DOCKER_IMAGE_NAME}:${DOCKER_RELEASE_VERSION}
+docker ps |grep ${DOCKER_RUN_NAME} 
+
+echo "Pushing ${DOCKER_REPOSITORY}/onap/oom/${DOCKER_IMAGE_NAME}:${DOCKER_RELEASE_VERSION}"
+docker push ${DOCKER_REPOSITORY}/onap/oom/${DOCKER_IMAGE_NAME}:${DOCKER_RELEASE_VERSION}
